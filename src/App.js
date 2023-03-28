374e2a33f50a2ec19269d7ac1a5d5eeaa19caf70
Patrik Tomek <pataz23@seznam.cz>
2023-03-28 17:58:36 +0200

    Getting data from firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { projectFirestore } from "./firebase/config";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
 
-export default App
+  useEffect(() => {
+    projectFirestore
+      .collection("superheroes")
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setError("Database is empty.");
+        } else {
+          let result = [];
+          snapshot.docs.forEach((supHero) => {
+            result.push({ id: supHero.id, ...supHero.data() });
+          });
+          setData(result);
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      {data.map((supHero) => {
+        const { id, name, superpower, age } = supHero;
+        return (
+          <div key={id}>
+            {name}, {superpower}, {age}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcf85_EHxki0Yl8-2ZSEMX_xc8xxGlyrw",
+  authDomain: "superhero-database-68d3f.firebaseapp.com",
+  projectId: "superhero-database-68d3f",
+  storageBucket: "superhero-database-68d3f.appspot.com",
+  messagingSenderId: "3477979952",
+  appId: "1:3477979952:web:a1ccb35512982dc96b3ef5",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
