5ea52d7ade2d07178a41b5374901ccb8391bf114
Patrik Tomek <pataz23@seznam.cz>
2023-03-30 18:19:38 +0200

    Adding heroes

--- src/components/SuperheroForm.js
+++ src/components/SuperheroForm.js
@@ -1,36 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { firestoreData } from "../firebase/config";
 
 const SuperheroForm = () => {
+  const [name, setSupName] = useState("");
+  const [superpower, setSuperpower] = useState("");
+  const [age, setSupAge] = useState(null);
+
+  const nameHandler = (e) => {
+    setSupName(e.target.value);
+  };
+  const powerHandler = (e) => {
+    setSuperpower(e.target.value);
+  };
+  const ageHandler = (e) => {
+    setSupAge(e.target.value);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const newSupHero = { name, superpower, age };
+    await firestoreData.collection("superheroes").add(newSupHero);
+    setSupName("");
+    setSuperpower("");
+    setSupAge("");
+  };
+
   return (
-    <form className="superhero-form">
+    <form className="superhero-form" onSubmit={submitHandler}>
       <label className="superhero-form-label" htmlFor="form-name">
         Name
       </label>
       <input
+        onChange={nameHandler}
+        value={name}
         className="superhero-form-input"
         id="form-name"
         type="text"
         placeholder="Name"
+        required
       />
       <label className="superhero-form-label" htmlFor="form-superpower">
         Superpower
       </label>
       <input
+        onChange={powerHandler}
+        value={superpower}
         className="superhero-form-input"
         id="form-superpower"
         type="text"
         placeholder="Superpower"
+        required
       />
       <label className="superhero-form-label" htmlFor="form-age">
         Age
       </label>
       <input
+        onChange={ageHandler}
+        value={age}
         className="superhero-form-input"
         id="form-age"
         type="number"
         placeholder="Estimated age"
+        min="0"
       />
-      <button className="superhero-form-btn">Add</button>
+      <button className="superhero-form-btn" type="submit">
+        Add
+      </button>
     </form>
   );
 };
